d27346c2e99539df252c5e43945e79d2f9b346ab
dzonimn <imnwby@outlook.com>
2021-03-06 21:01:25 +0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.15

---
+++ src/HoJDiscordBot.jl
@@ -0,0 +1,5 @@
+module HoJDiscordBot
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HoJDiscordBot
+using Test
+
+@testset "HoJDiscordBot.jl" begin
+    # Write your tests here.
+end
